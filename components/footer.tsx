2717606d91bc21ec76454f59fce444e7812b2d9f
vinhtrinh1 <vinht@bu.edu>
2025-03-18 21:46:48 -0400

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,8 +5,6 @@ const Footer = () => {
     <footer className="bg-gray-100 py-8">
       <div className="container mx-auto px-6">
         <div className="flex flex-col md:flex-row justify-between items-center">
-          <p className="text-gray-600 mb-4 md:mb-0">
-          </p>
           <div className="flex space-x-6">
             <a
               href="https://www.linkedin.com/in/vinh-trinh/"
